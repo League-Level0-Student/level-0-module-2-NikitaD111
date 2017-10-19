588e2c82df4ca62dc762e148f4c2ed7d25d9144a
ColeTrammer <cole.trammer@gmail.com>
2017-10-18 18:24:22 -0700

    badgers maybes unbirthday mustaches #2

---
+++ src/Unbirthday.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+public static void main(String[] args) {
+	JOptionPane.showInputDialog("when is your birthday");
+if (condition) {
+	
+}
+	
+	
+	
+	
+}
+}
