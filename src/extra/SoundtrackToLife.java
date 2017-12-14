8398ce6bb28e0bfff01cd015480b62eec8276477
67tasman <league@WTS14.attlocal.net>
2017-12-13 18:27:42 -0800

    Soundtracktolife,LotteryNumbers,RollerCoaster,VotingBooth

--- src/extra/SoundtrackToLife.java
+++ src/extra/SoundtrackToLife.java
@@ -4,6 +4,7 @@
 package extra;
 
 import java.net.URI;
+
 import javax.swing.JOptionPane;
 
 public class SoundtrackToLife {
@@ -11,27 +12,31 @@ public class SoundtrackToLife {
 	public static void main(String[] args) {
 
 		// 1. Adjust this pop-up to find out what mood the user is in.
-		int userMood = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Mood1", "Mood2", "Mood3" }, null);
-		
+		int userMood = JOptionPane.showOptionDialog(null, "How do you feel", "Pop-up Title", 0,
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "happy", "sad", "angry" }, null);
+
 		// 2. Their answer is stored in the userMood variable. Print it out.
-
-		// 3. If they are in a stressed mood, use the playVideo method to play a calming song from YouTube.
-		
-
-		
+		System.out.println(userMood);
+		// 3. If they are in a stressed mood, use the playVideo method to play a calming
+		// song from YouTube.
+		if (userMood == 2) {
+			playVideo("bit.ly/video-for-happy");
+		} else if (userMood == 1) {
+			playVideo("bit.ly/video-for-sad");
+		} else if (userMood == 0) {
+			playVideo("bit.ly/video-for-angry");
+		}
 		// 4. Play different songs for other moods.
 
-/**
-* If you can’t think of any, here are some you can use...
-*	http://bit.ly/video-for-happy
-*	http://bit.ly/video-for-sad
-*	http://bit.ly/video-for-angry
-**/
-		
-		// If you are seeing ads at the beginning of your videos, install Adblock.
-
 	}
+
+	/**
+	 * If you can’t think of any, here are some you can use...
+	 * http://bit.ly/video-for-happy http://bit.ly/video-for-sad
+	 * http://bit.ly/video-for-angry
+	 **/
+
+	// If you are seeing ads at the beginning of your videos, install Adblock.
 
 	static void playVideo(String youTubeLink) {
 		try {
@@ -42,6 +47,3 @@ public class SoundtrackToLife {
 		}
 	}
 }
-
-
-
